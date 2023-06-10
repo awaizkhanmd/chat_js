f4cbd3d388f072e01f9e1e4194eb8324aa9ae851
MD AWAIZ <awaizkhanmd@gmail.com>
2023-06-11 01:45:45 +0530

    25:00

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { useState, useCallback } from 'react'
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
+type Variannt = 'LOGIN' | 'REGISTER'
+
+export default function AuthForm() {
+  const [variant, setVariant] = useState<Variannt>('LOGIN')
+
+  const [isLoading, setIsloading] = useState(false);
+  const toggleVariant = useCallback(
+    () => {
+      if (variant === 'LOGIN') {
+        setVariant('REGISTER');
+      }
+      else {
+        setVariant('LOGIN')
+      }
+    }, [variant]);
+  const { register, handleSubmit, formState: { errors } } = useForm<FieldValues>({
+
+    defaultValues: {
+      name: "",
+      email: "",
+      password: ""
+    }
+  });
+
+
+  const onSumbit: SubmitHandler<FieldValues> = (data) => {
+    setIsloading(true);
+    if (variant === 'REGISTER') {
+      //Axios Registering 
+    }
+    if (variant === 'LOGIN') {
+      //Login
+    }
+  }
+
+  const socialAction = (action: string) => {
+    setIsloading(true);
+    //Social sign
+  }
+
+  return (
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md' >
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm-px-10'>
+<form action=''></form>
+      </div>
+    </div>
+  )
+}
